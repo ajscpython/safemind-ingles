edd635a52d123d554ab319cb646ef18f67809788
ajscpython <99674497+ajscpython@users.noreply.github.com>
2025-04-19 18:38:57 -0300

    Delete sobre.js

--- sobre.js
+++
@@ -1,9 +0,0 @@
-export default function Sobre() {
-  return (
-    <div className="container">
-      <h1>Sobre o Curso</h1>
-      <p>O Safemind-Inglês é um curso desenvolvido para promover o aprendizado consciente e eficaz da língua inglesa, integrando técnicas de foco mental e desenvolvimento pessoal.</p>
-      <p>Indicado para iniciantes e intermediários, com foco em evolução real e aplicação prática da língua.</p>
-    </div>
-  );
-}
