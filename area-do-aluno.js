40ed6dbc71425dbeb3a40630990167989ec9dbe2
ajscpython <99674497+ajscpython@users.noreply.github.com>
2025-04-19 18:37:35 -0300

    Delete area-do-aluno.js

--- area-do-aluno.js
+++
@@ -1,8 +0,0 @@
-export default function AreaDoAluno() {
-  return (
-    <div className="container">
-      <h1>Área do Aluno</h1>
-      <p>Em breve, você poderá acessar seu painel com aulas, pagamentos e histórico.</p>
-    </div>
-  );
-}
